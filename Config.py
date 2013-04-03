dd32a447ab369d362326fee26ae874384b429d5b
Pavol Antalik <pavol.antalik@zuno.eu>
2013-04-03 20:59:04 +0200

    Quick and dirty testing application

---
+++ Config.py
@@ -0,0 +1,10 @@
+'''
+Created on 6.7.2012
+
+@author: PA
+'''
+
+DB = '../Data/inferno10-sqlite-2.db'
+
+if __name__ == '__main__':
+    pass
---
+++ Main.py
@@ -0,0 +1,49 @@
+
+'''
+Created on 6.7.2012
+
+@author: PA
+'''
+
+import EveDB
+import EveCentral
+import locale
+from Config import *
+
+locale.setlocale(locale.LC_ALL, "us_us.1250")
+
+ME = 0
+productionEfficiency = 5
+
+
+def main():
+#    invType1 = EveDB.EveItem(DB, 0, typeName='Abaddon')
+    invType1 = EveDB.EveItem(DB, 0, typeName='Anshar')
+#    invType1 = EveDB.EveItem(DB, 0, typeName='Orca')
+#    invType1 = EveDB.EveItem(DB, 24692, typeName='')
+
+    ec = EveCentral.EveCentral()
+    print 'Product Name: %s, ID: %s' % (invType1.typeName, invType1.typeID)
+    print 'Parent  BlueprintTypeID: %s' % (invType1.parentBlueprintTypeID)
+
+    materialList = invType1.getProduceMaterialIDList()
+    materialAmountList = invType1.getManufacturingMaterialAmountList(ME, productionEfficiency)
+
+#    ECresult = ec.getItemDataByTypeID(materialList)
+
+    for materialType, materialAmount in materialAmountList:
+        material = EveDB.EveItem(DB, materialType)
+        print "Amount of %s-%s: %s" % (material.typeID, material.typeName, locale.format("%d", materialAmount, True))
+        #=======================================================================
+        # if material.blueprintTypeID:
+        #    subMaterialAmountList = material.getManufacturingMaterialAmountList(ME, productionEfficiency)
+        #    for subMaterialType, subMaterialAmount in subMaterialAmountList:
+        #        subMaterial = EveDB.EveItem(DB, subMaterialType)
+        #        print ".... Amount of %s-%s: %s" % (subMaterial.typeID, subMaterial.typeName, locale.format("%d", subMaterialAmount, True))
+        #=======================================================================
+
+#        print "buy max: %s" % locale.format("%d", ec.parseItemData(ECresult, materialType, 'buy', 'max'), True)
+
+
+if __name__ == '__main__':
+    main()
