043af598a77ebc81047d6503a65aace2318e7820
Pavol Antalik <pavol.antalik@zuno.eu>
2013-04-05 22:57:06 +0200

    Changed path of DB file

--- Config.py
+++ Config.py
@@ -4,7 +4,8 @@ Created on 6.7.2012
 @author: PA
 '''
 
-DB = '../Data/inferno10-sqlite-2.db'
+#DB = '../Data/inferno10-sqlite-2.db'
+DB = 'd:/Program Files/eclipse.workspace.2/Data/Blueprints/eve-inferno10-blueprints-sqlite-2.db'
 
 if __name__ == '__main__':
     pass
