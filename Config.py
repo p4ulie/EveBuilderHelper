692281ac565afb4e57a9cfd4b6761fd81f58cb2e
Pavol Antalik <pavol.antalik@zuno.eu>
2013-04-07 23:56:52 +0200

    Use lightweight DB version (ItemType and Blueprint related tables)

--- Config.py
+++ Config.py
@@ -5,7 +5,8 @@ Created on 6.7.2012
 '''
 
 #DB = '../Data/inferno10-sqlite-2.db'
-DB = 'd:/Program Files/eclipse.workspace.2/Data/Blueprints/eve-inferno10-blueprints-sqlite-2.db'
+#DB = 'd:/Program Files/eclipse.workspace.2/Data/Blueprints/eve-inferno10-blueprints-sqlite-2.db'
+DB = 'd:/Program Files/eclipse.workspace.2/Data/Blueprints/bp.db'
 
 if __name__ == '__main__':
     pass
