137a96e4b7219e5f0e32a75fdd0d90ad02bc9b57
Pavol Antalik <pavol.antalik@zuno.eu>
2013-05-30 18:37:13 +0200

    Code cleanup

--- Main.py
+++ Main.py
@@ -5,10 +5,12 @@ Created on 6.7.2012
 @author: PA
 '''
 
-import EveItem
+from EveItem import *
+from EveBlueprint import *
 import EveCentral
 import locale
 from Config import *
+import sys
 
 locale.setlocale(locale.LC_ALL, "us_us.1250")
 
@@ -17,33 +19,26 @@ productionEfficiency = 5
 
 
 def main():
-#    invType1 = EveItem.EveItem(DB, 0, typeName='Abaddon')
-    invType1 = EveItem.EveItem(DB, 0, typeName='Anshar')
-#    invType1 = EveItem.EveItem(DB, 0, typeName='Orca')
-#    invType1 = EveItem.EveItem(DB, 24692, typeName='')
+#    item1 = EveItem.EveItem(DB, 0, typeName='Abaddon')
+    item1 = EveItem(DB, typeName='Anshar')
+    blueprint1 = EveBlueprint(DB, productTypeID = item1.typeID)
+#    item1 = EveItem.EveItem(DB, 0, typeName='Orca')
+#    item1 = EveItem.EveItem(DB, 24692, typeName='')
 
     ec = EveCentral.EveCentral()
-    print 'Product Name: %s, ID: %s' % (invType1.typeName, invType1.typeID)
-    print 'Parent  BlueprintTypeID: %s' % (invType1.parentBlueprintTypeID)
+    print 'Product Name: %s, ID: %s' % (item1.typeName, item1.typeID)
+    print 'Parent  BlueprintTypeID: %s' % (blueprint1.blueprintTypeID)
 
-    materialList = invType1.getProduceMaterialIDList()
-    materialAmountList = invType1.getManufacturingMaterialAmountList(ME, productionEfficiency)
+    
+    materialList = blueprint1.getManufacturingMaterials()
+
+    sys.exit()
 
 #    ECresult = ec.getItemDataByTypeID(materialList)
 
-    for materialType, materialAmount in materialAmountList:
-        material = EveItem.EveItem(DB, materialType)
+    for materialType, materialAmount in materialList:
+        material = EveItem(DB, materialType)
         print "Amount of %s-%s: %s" % (material.typeID, material.typeName, locale.format("%d", materialAmount, True))
-        #=======================================================================
-        # if material.blueprintTypeID:
-        #    subMaterialAmountList = material.getManufacturingMaterialAmountList(ME, productionEfficiency)
-        #    for subMaterialType, subMaterialAmount in subMaterialAmountList:
-        #        subMaterial = EveItem.EveItem(DB, subMaterialType)
-        #        print ".... Amount of %s-%s: %s" % (subMaterial.typeID, subMaterial.typeName, locale.format("%d", subMaterialAmount, True))
-        #=======================================================================
-
-#        print "buy max: %s" % locale.format("%d", ec.parseItemData(ECresult, materialType, 'buy', 'max'), True)
-
 
 if __name__ == '__main__':
     main()
