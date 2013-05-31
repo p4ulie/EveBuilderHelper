998b5a3f204d7d439ac30b98fd4972b415dcadf2
Pavol Antalik <pavol.antalik@zuno.eu>
2013-05-31 07:14:43 +0200

    Full DB dump used again (until required minimum set of tables will be determined)table

--- Config.py
+++ Config.py
@@ -4,8 +4,8 @@ Created on 6.7.2012
 @author: Pavol Antalik
 '''
 
-#DB = '../Data/inferno10-sqlite-2.db'
-DB = 'd:/Program Files/eclipse.workspace.2/Data/Blueprints/bp.db'
+DB = '../Data/inferno10-sqlite-2.db'
+#DB = 'd:/Program Files/eclipse.workspace.2/Data/Blueprints/bp.db'
 
 if __name__ == '__main__':
     pass
