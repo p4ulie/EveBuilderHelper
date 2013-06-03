8317b87dba5e8c563508fd581b711258d5d4d7dd
Paulie <paulie@homeserver.LAN>
2013-06-03 09:28:02 +0200

    Locale setup for all OS

--- Main.py
+++ Main.py
@@ -12,23 +12,24 @@ import locale
 from Config import *
 import sys
 
-locale.setlocale(locale.LC_ALL, "us_us.1250")
+if (sys.platform == 'win32'):
+    locale.setlocale(locale.LC_ALL, "us_us.1250")
+elif (sys.platform == 'darwin'):
+    locale.setlocale(locale.LC_ALL, "en_US")
+elif (sys.platform == 'linux2'):
+    locale.setlocale(locale.LC_ALL, "en_US.UTF-8")
 
 ME = 0
 productionEfficiency = 5
 
 
 def main():
-#    item1 = EveItem.EveItem(DB, 0, typeName='Abaddon')
     item1 = EveItem(DB, typeName='Anshar')
     blueprint1 = EveBlueprint(DB, productTypeID = item1.typeID)
-#    item1 = EveItem.EveItem(DB, 0, typeName='Orca')
-#    item1 = EveItem.EveItem(DB, 24692, typeName='')
 
     ec = EveCentral.EveCentral()
     print 'Product Name: %s, ID: %s' % (item1.typeName, item1.typeID)
     print 'Parent  BlueprintTypeID: %s' % (blueprint1.blueprintTypeID)
-    
 
     materialList = blueprint1.getManufacturingMaterials()
 
