483b1281dcfd5d8069b733cf478f37c3b0f2d9e2
Paulie <paulie@homeserver.LAN>
2013-08-18 21:45:30 +0200

    Output lines only wit price lower than Jita max buy, line formating edited for better readability (still not perfect)

--- Demo/MaterialPriceFinder.py
+++ Demo/MaterialPriceFinder.py
@@ -45,15 +45,21 @@ def main():
     materialList = bp.getManufacturingMaterials(characterPESkillLvl=characterPESkillLvl)
     for material in materialList.keys():
         # Get Jita price
-        result = ec.marketstatGet(material, useSystem=solarSys.solarSystemID)
-        jita_buy_max = ec.marketstatParse(result, material, 'buy', 'max')
+        matItem = EveItem(DB, itemID=material)
+        print "Getting Jita price for item %s..." % matItem.name
+        resultJita = ec.marketstatGet(material, useSystem=solarSys.solarSystemID)
+        priceJitaBuyMax = float(ec.marketstatParse(resultJita, material, 'buy', 'max'))
+        print "Jita price for item %s is %s (max buy)\n" % (matItem.name, priceJitaBuyMax)
         
-        # self, typeIDList, setHours, regionLimit, useSystem, setMinQ, orderType    
+        print "Getting trade hub prices for item %s..." % matItem.name
         result = ec.quicklookGetList(material, regionLimit=regionIDList, orderType='sell_orders')
     
         for line in result:
             if line[4] in tradeHubList.values():
-                print line
+                if float(line[7]) < priceJitaBuyMax:
+                    print "Item: %s, station: %s, price: %f, quantity: %s" % (line[1], line[4], float(line[7]), line[8])
+
+        print "\n"
     
 
 if __name__ == '__main__':
