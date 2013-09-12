b42d6a7e87491938e517bc462608ce1c4ce3827c
Pavol Antalik <avol.antalik@gmail.com>
2013-09-12 17:46:32 +0200

    Module to handle Eve Market data download and processing

---
+++ EveModules/EveMarketData.py
@@ -0,0 +1,92 @@
+'''
+Class for Eve Market data fetching and storing 
+Using sqlite as storage
+
+Created on 12.9.2013
+
+@author: PA
+'''
+
+import sqlite3
+from EveModules.EveCentral import *
+
+marketDataSources = ('EveCentral')
+
+class EveMarketData(object):
+    '''
+    Class for Eve Market data fetching and storing 
+    '''
+    
+    def fetchOrdersEveCentral(self, itemID, regionLimit='', orderType='sell_orders'):
+        '''
+        Fetch data from Eve-Central  
+        '''
+
+        ec = EveCentral()
+        result = ec.quicklookGetList(itemID, regionLimit=regionLimit, orderType=orderType)
+        return result
+
+    def fetchOrders(self, itemID='', regionLimit='', orderType='sell_orders', marketDataSource='EveCentral'):
+        '''
+        Fetch data from various Market pages (Eve-Central, Eve market Data, ...)  
+        '''
+
+        if marketDataSource == 'EveCentral':
+            result = self.fetchOrdersEveCentral(itemID, regionLimit=regionLimit, orderType=orderType)
+
+        for line in result:
+            print format(line)
+#             self.cur.execute("""INSERT INTO prices
+#                             (itemID,
+#                             itemName,
+#                             regionID,
+#                             stationID,
+#                             stationName,
+#                             security,
+#                             range,
+#                             price,
+#                             volRemain,
+#                             minVolume,
+#                             expires,
+#                             reportedTime,
+#                             orderType,
+#                             jitaBuyMax)
+#                             values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, 'sell', ?)
+#             """, (line[0], line[1], line[2], line[3], line[4], line[5], line[6], line[7],
+#                   line[8], line[9], line[10], line[11], priceJitaBuyMax)
+#             )
+
+    def __init__(self):
+        '''
+        Create a DB connection
+        '''
+        self.con = sqlite3.connect(":memory:")
+        self.con.text_factory = str
+        self.cur = self.con.cursor()
+        self.cur.executescript("""
+            create table if not exists prices(
+                itemID integer,
+                itemName text,
+                regionID integer,
+                stationID integer,
+                stationName text,
+                security real,
+                range integer,
+                price real,
+                volRemain integer,
+                minVolume integer,
+                expires text,
+                reportedTime text,
+                orderType text,
+                jitaBuyMax real,
+                timeStampFetch integer
+            );
+            """)
+        self.con.commit()
+    
+if __name__ == '__main__':
+    emd = EveMarketData()
+    
+    TritaniumID = 34
+    emd.fetchOrders(itemID = TritaniumID)
+    
