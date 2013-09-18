22b58b39bb0b2796cdd80a91b34285df29ebd5c1
Pavol Antalik <pavol.antalik@zuno.eu>
2013-09-18 19:34:37 +0200

    Get and locally store values in case cached ones in DB are older than default amount of minutes (60)

--- EveModules/EveMarketData.py
+++ EveModules/EveMarketData.py
@@ -11,6 +11,8 @@ import sqlite3
 import time
 from EveModules.EveCentral import *
 
+OLDERTHANMINUTES = 1
+
 defaultDBFile = 'marketdata.db'
 marketDataSources = ('EveCentral')
 
@@ -19,52 +21,82 @@ class EveMarketData(object):
     Class for Eve Market data fetching and storing 
     '''
     
-    def fetchOrdersEveCentral(self, itemID, regionLimit='', orderType='sell_orders'):
+    def fetchOrdersFromEveCentral(self, itemID, regionLimit='', orderType='sell_orders'):
         '''
         Fetch data from Eve-Central  
         '''
 
         ec = EveCentral()
         result = ec.quicklookGetList(itemID, regionLimit=regionLimit, orderType=orderType)
+
+        ts = time.time()
+        for line in result:
+            self.cur.execute("""INSERT INTO prices
+                            (itemID,
+                            itemName,
+                            regionID,
+                            stationID,
+                            stationName,
+                            security,
+                            range,
+                            price,
+                            volRemain,
+                            minVolume,
+                            expires,
+                            reportedTime,
+                            orderType,
+                            timeStampFetch)
+                            values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+            """, (line[0], line[1], line[2], line[3], line[4], line[5], line[6], line[7],
+                  line[8], line[9], line[10], line[11], orderType, ts)
+            )
+        self.con.commit()
+        
         return result
 
-    def fetchOrders(self, itemID='', regionLimit='', orderType='sell_orders', marketDataSource='EveCentral'):
+    def deleteAllOrders(self):
+        self.cur.execute("""DELETE FROM prices""")
+        self.con.commit()
+        
+    def deleteOldOrders(self, minutes=OLDERTHANMINUTES):
+        ts = time.time()
+        olderThanMinutes = ts - (minutes * 60)
+        self.cur.execute("""DELETE FROM prices WHERE timeStampFetch < (olderThanMinutes)
+                            values (?)""")
+        self.con.commit()
+
+    def areThereOrderOlderThanMinutes(self, itemID = '', minutes=OLDERTHANMINUTES):
+        if itemID:
+            ts = time.time()
+            olderThanMinutes = ts - (minutes * 60)
+            self.cur.execute("""SELECT count(*) FROM prices WHERE itemID = (?) AND timeStampFetch < (olderThanMinutes)
+                                values (?)""")
+            result = self.cur.fetchone()
+            if result:
+                if result > 0:
+                    return True
+                else:
+                    return False
+            else:
+                return False
+        return False
+    
+    def fetchOrders(self, itemID='', ifOlderThanMinutes = OLDERTHANMINUTES, regionLimit='', orderType='sell_orders', marketDataSource='EveCentral'):
         '''
         Fetch data from various Market pages (Eve-Central, Eve market Data, ...)  
         '''
-
-        if marketDataSource == 'EveCentral':
-            result = self.fetchOrdersEveCentral(itemID, regionLimit=regionLimit, orderType=orderType)
-
-        for line in result:
-            print format(line)
-            ts = time.time()
-            print ts
-#             self.cur.execute("""INSERT INTO prices
-#                             (itemID,
-#                             itemName,
-#                             regionID,
-#                             stationID,
-#                             stationName,
-#                             security,
-#                             range,
-#                             price,
-#                             volRemain,
-#                             minVolume,
-#                             expires,
-#                             reportedTime,
-#                             orderType,
-#                             jitaBuyMax)
-#                             values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, 'sell', ?)
-#             """, (line[0], line[1], line[2], line[3], line[4], line[5], line[6], line[7],
-#                   line[8], line[9], line[10], line[11], priceJitaBuyMax)
-#             )
+        
+        # only update data if DB entries got older than ifOlderThanMinutes
+        if not self.areThereOrderOlderThanMinutes(itemID=itemID, ifOlderThanMinutes=ifOlderThanMinutes):
+            if marketDataSource == 'EveCentral':
+                if self.fetchOrdersFromEveCentral(itemID, regionLimit=regionLimit, orderType=orderType):
+                    # if we have some new data, delete all older
+                    self.deleteOldOrders(ifOlderThanMinutes)
 
     def __init__(self, dbFile=defaultDBFile):
         '''
         Create a DB connection
         '''
-#        self.con = sqlite3.connect(":memory:")
         self.con = sqlite3.connect(dbFile)
         self.con.text_factory = str
         self.cur = self.con.cursor()
@@ -83,8 +115,7 @@ class EveMarketData(object):
                 expires text,
                 reportedTime text,
                 orderType text,
-                jitaBuyMax real,
-                timeStampFetch integer
+                timeStampFetch real
             );
             """)
         self.con.commit()
@@ -92,6 +123,7 @@ class EveMarketData(object):
 if __name__ == '__main__':
     emd = EveMarketData()
     
-    TritaniumID = 34
-    emd.fetchOrders(itemID = TritaniumID)
+    Tritanium_ID = 34
+    Strong_Blue_Pill_Booster_ID = 10156
+    emd.fetchOrders(itemID = Strong_Blue_Pill_Booster_ID, ifOlderThanMinutes = OLDERTHANMINUTES)
     
