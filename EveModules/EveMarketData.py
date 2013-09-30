d62ac1c932e854810a2fe27e5fe7665be92c3348
Pavol Antalik <pavol.antalik@zuno.eu>
2013-09-30 20:31:01 +0200

    Data are written in DB only in case there actually IS data

--- EveModules/EveMarketData.py
+++ EveModules/EveMarketData.py
@@ -66,31 +66,32 @@ class EveMarketData(object):
         ec = EveCentral()
         result = ec.quicklookGetList(**kwargs)
 
-        ts = time.time()
-
-        data = []
-        for line in result:
-            data.append((line[0], line[1], line[2], line[3], line[4], line[5], line[6], line[7], line[8], line[9], line[10], line[11], line[12], ts)
-                        )
-#        for line in result:
-        query = """INSERT INTO prices
-                        (itemID,
-                        itemName,
-                        regionID,
-                        stationID,
-                        stationName,
-                        security,
-                        range,
-                        price,
-                        volRemain,
-                        minVolume,
-                        expires,
-                        reportedTime,
-                        orderType,
-                        timeStampFetch)
-                        values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-        """
-        data = self.execQuery(query, param=data, executemany=True, commit=True)
+        if result:
+            ts = time.time()
+    
+            data = []
+            for line in result:
+                data.append((line[0], line[1], line[2], line[3], line[4], line[5], line[6], line[7], line[8], line[9], line[10], line[11], line[12], ts)
+                            )
+    #        for line in result:
+            query = """INSERT INTO prices
+                            (itemID,
+                            itemName,
+                            regionID,
+                            stationID,
+                            stationName,
+                            security,
+                            range,
+                            price,
+                            volRemain,
+                            minVolume,
+                            expires,
+                            reportedTime,
+                            orderType,
+                            timeStampFetch)
+                            values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+            """
+            data = self.execQuery(query, param=data, executemany=True, commit=True)
             
         return result
 
