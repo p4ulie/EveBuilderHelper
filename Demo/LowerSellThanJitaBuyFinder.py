52b64d275c9da5982dacd385b3367b7372034503
Pavol Antalik <pavol.antalik@zuno.eu>
2013-09-30 20:29:48 +0200

    Group of items supported (like for Minerals)

--- Demo/LowerSellThanJitaBuyFinder.py
+++ Demo/LowerSellThanJitaBuyFinder.py
@@ -7,6 +7,10 @@ Created on 27.9.2013
 import locale
 locale.setlocale(locale.LC_ALL, '')
 
+import sys
+
+from EveModules.EveItem import * 
+from EveModules.EveGroup import * 
 from EveModules.EveMarketData import *
 
 if __name__ == '__main__':
@@ -19,29 +23,37 @@ if __name__ == '__main__':
     emd = EveMarketData()
 
 #    emd.deleteAllOrders()
+
+    DB = '../../data/ody100-sqlite3-v1.db'
+    eGroup = EveGroup(DB, groupName = 'Mineral')
+    eItem = EveItem(DB)
     
-    result = emd.getMinSellOrderPrice(itemID = itemID, stationName=JITA_STATION_NAME)
-    print "Min sell order Jita: %s" % result
-
-    jitaMaxBuy = emd.getMaxBuyOrderPrice(itemID = itemID, stationName=JITA_STATION_NAME)
-    print "Max buy order Jita: %s" % jitaMaxBuy
-
-    query = '''
-    select itemName, stationName, price, volRemain, round((? - price) * volRemain) as saving
-    from prices
-    where
-    orderType = 'sell_orders' and
-    itemID = ? and
-    price < ? and
-    security > 0 and
-    saving > 10000000
-    order by saving desc
-    '''
-    param = (jitaMaxBuy, itemID, jitaMaxBuy)
+    for item in eItem.getItemsList(eGroup.groupID):
+        itemID = item[0]
+        print "Getting data for %s..." % item[2]
     
-    orders = emd.execQuery(query, param)
-
-    print "List of sell orders (low and highsec) lower than Jita buy:"
+        result = emd.getMinSellOrderPrice(itemID = itemID, stationName=JITA_STATION_NAME)
+        print "Min sell order Jita: %s" % result
+    
+        jitaMaxBuy = emd.getMaxBuyOrderPrice(itemID = itemID, stationName=JITA_STATION_NAME)
+        print "Max buy order Jita: %s" % jitaMaxBuy
+    
+        query = '''
+        select itemName, stationName, price, volRemain, round((? - price) * volRemain) as saving
+        from prices
+        where
+        orderType = 'sell_orders' and
+        itemID = ? and
+        price < ? and
+        security > 0 and
+        saving > 10000000
+        order by saving desc
+        '''
+        param = (jitaMaxBuy, itemID, jitaMaxBuy)
         
-    for order in orders:
-        print "Price: %.2f, Saving: '%s' for amount: '%s' in station: %s" % (order[2], locale.format('%d', order[4], 1), locale.format('%d', order[3], 1), order[1])
+        orders = emd.execQuery(query, param)
+    
+        print "List of sell orders (low and highsec) lower than Jita buy:"
+            
+        for order in orders:
+            print "Price: %.2f, Saving: '%s' for amount: '%s' in station: %s" % (order[2], locale.format('%d', order[4], 1), locale.format('%d', order[3], 1), order[1])
