2ab7f3a54400e0a18b30054609b65e40934b2660
Pavol Antalik <pavol.antalik@gmail.com>
2013-10-16 08:08:26 +0200

    Class prototypes for manufacturing - Project and Task

---
+++ EveManufacturing/EveManufacturingProject.py
@@ -0,0 +1,17 @@
+'''
+Created on Oct 13, 2013
+
+@author: paulie
+'''
+
+class EveManufacturingProject(object):
+    '''
+    Class for manufacturing projects in Eve Online
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        
---
+++ EveManufacturing/EveManufacturingTask.py
@@ -0,0 +1,17 @@
+'''
+Created on Oct 13, 2013
+
+@author: paulie
+'''
+
+class EveManufacturingClass(object):
+    '''
+    Class for manufacturing tasks in Eve Online
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        
