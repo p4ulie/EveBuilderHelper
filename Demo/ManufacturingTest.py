a3dc2ea1513271454c18425e32494ce00801354a
Pavol Antalik <pavol.antalik@gmail.com>
2013-10-26 13:13:20 +0200

    Output - task names list and project material list

--- Demo/ManufacturingTest.py
+++ Demo/ManufacturingTest.py
@@ -12,30 +12,34 @@ from Config import *
 
 def main():
     
-    proj = EveManufacturingProject(name='Ishtar Project')
+    proj = EveManufacturingProject(name='Ishtar  Project')
 
-    invType = EveInvType(DB, name='Mjolnir Fury Light Missile')
-    bp = EveInvBlueprintType(DB, productID = invType.getTypeID(), ResearchLevelME = -4, ResearchLevelPE = -4)
-
-    task = proj.addTask(blueprint=bp)
-    task.setQuantity(2)
+    invType = EveInvType(name='Ishtar')
+    bp = EveInvBlueprintType(productID = invType.typeID, ResearchLevelME = -4, ResearchLevelPE = -4)
 
     task = proj.addTask(blueprint=bp)
+    task.manufacturingQuantity = 10
 
-    print "Number of task in project: %d" % proj.getTaskCount()
+#    task = proj.addTask(blueprint=bp)
 
-    print "List of tasks in project: "
+    print "Number of task in project: %d\n" % proj.getTaskCount()
+    print "List of tasks in project:\n"
     for task in proj.getTaskList():
-        matList = [(mat.getTypeID(), mat.getTypeName(), mat.getQuantity()) for mat in task.getMaterialList()]
-        print "%s:" % task.getName()
-        for mt in matList:
-            print "(%s) %s: %d" % (mt[0], mt[1], mt[2])
-        print
+        print "%s\n" % task.name
 
-#    print "Material list for project: "
-#    matList = [(mat.getTypeID(), mat.getTypeName(), mat.getQuantity()) for mat in proj.getMaterialList()]
-#     for mt in matList:
-#         print "(%s) %s: %d" % (mt[0], mt[1], mt[2])
+#     print "List of tasks in project:\n"
+#     for task in proj.getTaskList():
+#         taskMatList = [(taskMaterial.typeID, taskMaterial.typeName, taskMaterial.quantity) for taskMaterial in task.getMaterialList()]
+#         print "%s:" % task.name
+#         for tskMt in taskMatList:
+#             print "(%s) %s: %d" % (tskMt[0], tskMt[1], tskMt[2])
+# 
+#         print
+
+    print "Material list for project: "
+    projMatList = [(projMaterial.typeID, projMaterial.typeName, projMaterial.quantity) for projMaterial in proj.getMaterialList()]
+    for prjMt in projMatList:
+         print "(%s) %s: %d" % (prjMt[0], prjMt[1], prjMt[2])
     
 if __name__ == '__main__':
     main()
