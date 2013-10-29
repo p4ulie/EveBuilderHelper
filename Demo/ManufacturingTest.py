670f064f13d0eaa0537fec786ca84b4770c9ecc0
Pavol Antalik <pavol.antalik@zuno.eu>
2013-10-29 18:31:14 +0100

    Subtask generatino testing

--- Demo/ManufacturingTest.py
+++ Demo/ManufacturingTest.py
@@ -11,35 +11,30 @@ from EveManufacturing.EveManufacturingProject import EveManufacturingProject
 from Config import *
 
 def main():
-    
-    proj = EveManufacturingProject(name='Ishtar  Project')
+
+        
+    proj = EveManufacturingProject(name='Ishtar')
 
     invType = EveInvType(name='Ishtar')
     bp = EveInvBlueprintType(productID = invType.typeID, ResearchLevelME = -4, ResearchLevelPE = -4)
 
-    task = proj.addTask(blueprint=bp)
-    task.manufacturingQuantity = 10
+#    invType = EveInvType(name='425mm Railgun I')
+#    invType = EveInvType(name='Mjolnir Light Missile')
+#    bp = EveInvBlueprintType(productID = invType.typeID, ResearchLevelME = 27, ResearchLevelPE = 60)
 
-#    task = proj.addTask(blueprint=bp)
+    task = proj.addTask(name=invType.typeName, blueprint=bp, quantity = 1)
 
+    proj.addSubTasks(task, recursive=True)
+    
     print "Number of task in project: %d\n" % proj.getTaskCount()
     print "List of tasks in project:\n"
     for task in proj.getTaskList():
-        print "%s\n" % task.name
-
-#     print "List of tasks in project:\n"
-#     for task in proj.getTaskList():
-#         taskMatList = [(taskMaterial.typeID, taskMaterial.typeName, taskMaterial.quantity) for taskMaterial in task.getMaterialList()]
-#         print "%s:" % task.name
-#         for tskMt in taskMatList:
-#             print "(%s) %s: %d" % (tskMt[0], tskMt[1], tskMt[2])
-# 
-#         print
+        print "Manufacturing %s: %d" % (task.name, task.manufacturingQuantity)
 
     print "Material list for project: "
     projMatList = [(projMaterial.typeID, projMaterial.typeName, projMaterial.quantity) for projMaterial in proj.getMaterialList()]
-    for prjMt in projMatList:
-         print "(%s) %s: %d" % (prjMt[0], prjMt[1], prjMt[2])
-    
+#    for prjMt in projMatList:
+#         print "(%s) %s: %d" % (prjMt[0], prjMt[1], prjMt[2])
+
 if __name__ == '__main__':
     main()
