3c91da7ba92921774ae2696b2da03e895bd21a78
Pavol Antalik <pavol.antalik@zuno.eu>
2013-11-04 21:15:53 +0100

    Support for recursively adding subtasks

--- Demo/ManufacturingTest.py
+++ Demo/ManufacturingTest.py
@@ -11,30 +11,30 @@ from EveManufacturing.EveManufacturingProject import EveManufacturingProject
 from Config import *
 
 def main():
-
-        
+    # start new project
     proj = EveManufacturingProject(name='Ishtar')
 
+    # add task to project, based on blueprint
     invType = EveInvType(name='Ishtar')
     bp = EveInvBlueprintType(productID = invType.typeID, ResearchLevelME = -4, ResearchLevelPE = -4)
+    task = proj.addTask(name=invType.typeName, blueprint=bp, quantity = 1, recursive=True)
 
-#    invType = EveInvType(name='425mm Railgun I')
-#    invType = EveInvType(name='Mjolnir Light Missile')
-#    bp = EveInvBlueprintType(productID = invType.typeID, ResearchLevelME = 27, ResearchLevelPE = 60)
+    # add task to project, based on blueprint
+    invType = EveInvType(name='Myrmidon')
+    bp = EveInvBlueprintType(productID = invType.typeID, ResearchLevelME = 0, ResearchLevelPE = 0)
+    task = proj.addTask(name=invType.typeName, blueprint=bp, quantity = 1, recursive=True)
 
-    task = proj.addTask(name=invType.typeName, blueprint=bp, quantity = 1)
-
-    proj.addSubTasks(task, recursive=True)
-    
     print "Number of task in project: %d\n" % proj.getTaskCount()
     print "List of tasks in project:\n"
     for task in proj.getTaskList():
-        print "Manufacturing %s: %d" % (task.name, task.manufacturingQuantity)
+        print "Manufacturing %s: %d (priority: %d)" % (task.name, task.manufacturingQuantity, task.taskPriority)
 
-    print "Material list for project: "
-    projMatList = [(projMaterial.typeID, projMaterial.typeName, projMaterial.quantity) for projMaterial in proj.getMaterialList()]
-    for prjMt in projMatList:
-         print "(%s) %s: %d" % (prjMt[0], prjMt[1], prjMt[2])
+#     print
+#     
+#     print "Material list for project: "
+#     projMatList = [(projMaterial.typeID, projMaterial.typeName, projMaterial.quantity) for projMaterial in proj.getMaterialList()]
+#     for prjMt in projMatList:
+#         print "(%s) %s: %d" % (prjMt[0], prjMt[1], prjMt[2])
 
 if __name__ == '__main__':
     main()
