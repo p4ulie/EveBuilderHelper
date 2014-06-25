f61eab7d228a755ac4a947a1a1b3f92d1c426ca9
Pavol Antalik <pavol.antalik@zuno.eu>
2014-06-25 14:40:25 +0200

    Create object and connect immediately. Connection will be maintained after init.

--- DBAccessSQLite.py
+++ DBAccessSQLite.py
@@ -12,29 +12,46 @@ class DBAccessSQLite(object):
     Class for accessing SQlite database
     '''
 
+    DB = None
+    cur = None
+    dbcon = None
+    
     def __init__(self, DB):
         '''
         Constructor
         '''
         self.DB = DB
+        self.connect()
+        
+    def connect(self):
+        '''
+        Connect to database
+        '''
+        try:
+            self.dbcon = sqlite.connect(self.DB)
+            self.cur = self.dbcon.cursor()
+        except sqlite.Error, e:
+            print "Error %s:" % e.args[0]
+            sys.exit(1)
+        
+    def close(self):
+        '''
+        Close database connection
+        '''
+        if self.dbcon:
+            self.dbcon.close()
 
     def fetchData(self, query):
         '''
         Main method for accessing the DB
         '''
-        dbcon = ''
-        
         try:
-            dbcon = sqlite.connect(self.DB)
-            cur = dbcon.cursor()
-            cur.execute(query)
-            rows = cur.fetchall()
+            self.cur.execute(query)
+            rows = self.cur.fetchall()
         except sqlite.Error, e:
             print "Error %s:" % e.args[0]
+            self.close()
             sys.exit(1)
-        finally:
-            if dbcon:
-                dbcon.close()
     
         return rows
     
