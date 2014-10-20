0d9488b0ee22d18be4872c67ab5c8ebc42d4ea09
paulie <paulie@localhost.localdomain>
2014-10-20 20:47:50 +0200

    Various changes (I know, I know...)

--- Test_Blueprint.py
+++ Test_Blueprint.py
@@ -17,60 +17,72 @@ DB = 'data/eve.db'
 
 BUILD_PRODUCT_NAME = 'Kronos'
 BUILD_PRODUCT_COUNT = 1
-BUILD_PRODUCT_ME = 9
+BUILD_PRODUCT_ME = 10
 BUILD_PRODUCT_PE = 18
 
-ASSETS_LIST = '''Carbon\t1\tGeneral\t\t\t2 m3
-Construction Blocks\t13,430\tRefined Commodities\t\t\t20,145 m3
-Construction Components Capital\t\tFreight Container\t\t\t250,000 m3
-Crystalline Carbonide Armor Plate\t17,676\tConstruction Components\t\t\t17,676 m3
-Eifyr and Co. 'Alchemist' Neurotoxin Control NC-903\t1\tCyber Biology\t\t9\t 1 m3
-Enormous Freight Container\t8\tFreight Container\t\t\t20,000 m3
-Fermionic Condensates\t549\tComposite\t\t\t713.70 m3
-Ferrogel\t640\tComposite\t\t\t640 m3
-Fullerene Intercalated Sheets\t36\tHybrid Tech Components\t\t\t180 m3
-Fullerides\t49,695\tComposite\t\t\t7,454.25 m3
-Fusion Reactor Unit\t336\tConstruction Components\t\t\t336 m3
-Giant Freight Container\t10\tFreight Container\t\t\t12,000 m3
-Guidance Systems\t1,196\tSpecialized Commodities\t\t\t7,176 m3
-Hypersynaptic Fibers\t624\tComposite\t\t\t374.40 m3
-Ion Thruster\t561\tConstruction Components\t\t\t561 m3
-Isogen\t95,023\tMineral\t\t\t950.23 m3
-Magnetometric Sensor Cluster\t4,587\tConstruction Components\t\t\t4,587 m3
-Maller\t1\tCruiser\t\t\t10,000 m3
-Mechanical Parts\t65\tRefined Commodities\t\t\t97.50 m3
-Megacyte\t2,035\tMineral\t\t\t20.35 m3
-Metallofullerene Plating\t72\tHybrid Tech Components\t\t\t360 m3
-Mexallon\t3,329,832\tMineral\t\t\t33,298.32 m3
-Morphite\t13,880\tMineral\t\t\t138.80 m3
-Nanotransistors\t1,792\tComposite\t\t\t448 m3
-Nanowire Composites\t81\tHybrid Tech Components\t\t\t405 m3
-Nocxium\t24,525\tMineral\t\t\t245.25 m3
-Ore\t\tFreight Container\t\t\t250,000 m3
-Oscillator Capacitor Unit\t4,491\tConstruction Components\t\t\t4,491 m3
-Phenolic Composites\t3,470\tComposite\t\t\t694 m3
-Photon Microprocessor\t11,895\tConstruction Components\t\t\t11,895 m3
-Pulse Shield Emitter\t3,775\tConstruction Components\t\t\t3,775 m3
-Pyerite\t1,364,724\tMineral\t\t\t13,647.24 m3
-R.A.M.- Ammunition Tech\t6,300\tTool\t\t\t252 m3
-R.A.M.- Armor/Hull Tech\t200\tTool\t\t\t8 m3
-R.A.M.- Electronics\t5,300\tTool\t\t\t212 m3
-R.A.M.- Energy Tech\t200\tTool\t\t\t8 m3
-R.A.M.- Robotics\t96,900\tTool\t\t\t3,876 m3
-R.A.M.- Shield Tech\t200\tTool\t\t\t8 m3
-R.A.M.- Starship Tech\t65,880\tTool\t\t\t2,635.20 m3
-R.A.M.- Weapon Tech\t5,000\tTool\t\t\t200 m3
-Rifter\t6\tFrigate\t\t\t15,000 m3
-Robotics\t1,676\tSpecialized Commodities\t\t\t10,056 m3
-Sylramic Fibers\t133,256\tComposite\t\t\t6,662.80 m3
-Synth Blue Pill Booster Reaction\t1\tSimple Biochemical Reactions\t\t\t1 m3
-Terahertz Metamaterials\t1,976\tComposite\t\t\t1,976 m3
-Tristan\t4\tFrigate\t\t\t10,000 m3
-Tritanium\t6,818,891\tMineral\t\t\t68,188.91 m3
-Tungsten Carbide\t511,908\tComposite\t\t\t5,119.08 m3
-Venture\t11\tFrigate\t\t\t27,500 m3
-Zydrine\t7,431\tMineral\t\t\t74.31 m3
+ASSETS_LIST = '''Morphite\t13,010\tMineral\t
+Tritanium\t6,818,891\tMineral\t
+Magnetometric Sensor Cluster\t3,817\tConstruction Components\t
+Pulse Shield Emitter\t3385\tConstruction Components\t
+Fusion Reactor Unit\t193\tConstruction Components\t
 '''
+
+
+def build(type_id=None,
+            type_name=None,
+            blueprint_me=0,
+            runs=1,
+            asset_list=None):
+    '''
+    Calculate build/buy list
+    '''
+    eve_manufacturing = EveManufacturing.EveManufacturing(db_access_object, type_id=type_id, type_name=type_name)
+
+    facility_bonus_product = eve_manufacturing.get_bonuses_for_assembly_line_type(assembly_line_type_name="Component Assembly Array")['base_material_multiplier']
+
+    industrial_plan = eve_manufacturing.calculate_build(blueprint_me=blueprint_me,
+                                                       facility_bonus=facility_bonus_product,
+                                                       runs=runs,
+                                                       asset_list=asset_list)
+
+    for build_type_id, build_quantity in industrial_plan['build_list'].iteritems():
+        build(type_id=build_type_id,
+              blueprint_me=10,
+              runs=build_quantity,
+              asset_list=asset_list)
+
+    return industrial_plan
+
+
+def create_asset_list(line_list):
+    '''
+    Generate asset list from text lines
+    '''
+    asset_dict = {}
+
+    e_asset = EveManufacturing.EveManufacturing(db_access_object)
+
+    for line in line_list.splitlines():
+        match = re.match(r"^(\D+)\t([\d\,]*)\t(\D+)\t.*", line)
+        if match is not None:
+            type_name = match.group(1).strip()
+            quantity_string = match.group(2).replace(',','').strip()
+            if quantity_string == '':
+                quantity = 0
+            else:
+                quantity = int(quantity_string)
+            group_name = match.group(3).strip()
+
+            item = e_asset.get_inv_item(type_name=type_name)
+            if item is not None:
+                type_id = item["type_id"]
+                if type_id in asset_dict.iterkeys():
+                    asset_dict[type_id] += quantity
+                else:
+                    asset_dict[type_id] = quantity
+
+    return asset_dict
+
 
 def main():
     '''
@@ -82,40 +94,34 @@ def main():
 
     e_manuf = EveManufacturing.EveManufacturing(db_access_object, type_name=BUILD_PRODUCT_NAME)
 
-    facility_bonus_ship = e_manuf.get_bonus_for_assembly_line_type(assembly_line_type_name="Advanced Large Ship Assembly Array",
-                                                                             activity_id=EVE_ACTIVITY_MANUFACTURING)['base_material_multiplier']
+    asset_dict = create_asset_list(ASSETS_LIST)
+    asset_list = asset_dict.copy()
+    asset_list2 = asset_dict.copy()
 
-    facility_bonus_component = e_manuf.get_bonus_for_assembly_line_type(assembly_line_type_name="Components Assembly Array",
-                                                                        activity_id=EVE_ACTIVITY_MANUFACTURING)['base_material_multiplier']
+    build(type_name=BUILD_PRODUCT_NAME,
+          blueprint_me=BUILD_PRODUCT_ME,
+          runs=BUILD_PRODUCT_COUNT,
+          asset_list=asset_list2)
 
-    asset_dict = {}
-
-    for line in ASSETS_LIST.splitlines():
-        match = re.match(r"^(\D+)\t([\d\,]*)\t(\D+)\t.*", line)
-        if match is not None:
-            type_name = match.group(1).strip()
-            quantity_string = match.group(2).replace(',','').strip()
-            if quantity_string == '':
-                quantity = 0
-            else:
-                quantity = int(quantity_string)
-            group_name = match.group(3).strip()
-
-            item = e_manuf.get_inv_item(type_name=type_name)
-            if item is not None:
-                type_id = item["type_id"]
-                asset_dict[type_id] = quantity
-                #--------------- print "In assets %s\t%d" % (type_name,quantity)
-
-    print
-
-    industrial_plan = e_manuf.calculate_build_buy_list(activity_id=EVE_ACTIVITY_MANUFACTURING,
+    industrial_plan = e_manuf.calculate_build(activity_id=EVE_ACTIVITY_MANUFACTURING,
                                                        blueprint_me=blueprint_me,
-                                                       facility_bonus=facility_bonus_ship,
                                                        runs=BUILD_PRODUCT_COUNT,
-                                                       asset_list=asset_dict)
+                                                       asset_list=asset_list)
 
     print "Building: %s\n" % e_manuf.type_name
+
+    for material_type_id, material_quantity in industrial_plan['material_list'].iteritems():
+        material_name = e_manuf.get_inv_item(type_id=material_type_id)
+        if material_name is not None:
+
+            if material_type_id in asset_dict.iterkeys():
+                asset_quantity = asset_dict[material_type_id]
+            else:
+                asset_quantity = 0
+
+            print "Material %s\t%d (in assets %d)" % (material_name["type_name"], material_quantity, asset_quantity)
+
+    print
 
     for build_type_id, build_quantity in industrial_plan['build_list'].iteritems():
         build_name = e_manuf.get_inv_item(type_id=build_type_id)
@@ -129,14 +135,7 @@ def main():
         if buy_name is not None:
             print "Buy %s\t%d" % (buy_name["type_name"], buy_quantity)
 
-#===============================================================================
-#     print
-# 
-#     for type_id, quantity in industrial_plan['asset_list'].iteritems():
-#         name = e_manuf.get_inv_item(type_id=type_id)
-#         if name is not None:
-#             print "Assets left %s\t%d" % (name["type_name"], quantity)
-#===============================================================================
+    db_access_object.close()
 
 if __name__ == '__main__':
     db_access_object = DBAccessSQLite(DB)
