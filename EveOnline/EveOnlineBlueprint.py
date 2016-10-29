0b2124999a991c334a0c4ea9b2578e2a1385ce38
Pavol Antalik <pavol.antalik@gmail.com>
2016-10-30 01:23:53 +0200

    Class for blueprints introduced

---
+++ EveOnline/EveOnlineBlueprint.py
@@ -0,0 +1,39 @@
+'''
+Created on Oct 30, 2016
+
+@author: paulie
+'''
+
+
+from EveOnline.EveOnlineInvType import EveOnlineInvType
+
+
+class EveBlueprint(EveOnlineInvType):
+    '''
+    Class for data and methods for Blueprints in Eve Online
+    '''
+
+    # blueprint related attributes
+    blueprint_type_id = None   # determines whether item is buildable (blueprint exists as EveOnlineInvType)
+    blueprint_original = None  # True/False (Copy)
+    blueprint_me_level = 0
+    blueprint_te_level = 0
+    blueprint_runs = 0
+
+    def __init__(self,
+                 data_access,
+                 type_id=None,
+                 blueprint_original=None,
+                 blueprint_me_level=0,
+                 blueprint_te_level=0,
+                 build_runs=0):
+        '''
+        Constructor
+        '''
+
+        # initialize object and get basic data
+        EveOnlineInvType.__init__(self,
+                                  data_access,
+                                  type_id=type_id)
+
+        self.get_item(type_id=type_id)
