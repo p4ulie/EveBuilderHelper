e13d441ed7db99e73ac9f2ccfae539257d362108
Pavol Antalik <pavol.antalik@gmail.com>
2016-10-30 01:53:40 +0200

    EveOnlineCharacter Class introduced

---
+++ EveOnline/EveOnlineCharacter.py
@@ -0,0 +1,24 @@
+"""
+Created on Oct 28, 2016
+
+@author: paulie
+"""
+
+
+class EveOnlineInvCategory(object):
+    """
+    Class for data and methods for invCategories in Eve Online
+    """
+
+    def __init__(self):
+        """
+        Constructor
+        """
+
+        # basic attributes
+        self.character_name = ''
+        self.description = ''
+
+        # skills
+
+        # implants
