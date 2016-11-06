93858ac419a329b386edb9e72dbe1f3e0087b5ed
Pavol Antalík <pavol.antalik@gmail.com>
2016-11-06 19:58:42 +0100

    Code cleanup, Cost/Income/Profit calculations and output

--- Test_Blueprint.py
+++ Test_Blueprint.py
@@ -17,27 +17,31 @@ from EveOnline.EveOnlineRamAssemblyLineTypes import EveOnlineRamAssemblyLineType
 from DataAccess.MarketAccessEveCentral import MarketAccessEveCentral
 from DataAccess.EveMarket import EveMarket
 
+import locale
+locale.setlocale(locale.LC_ALL, 'en_US.utf8')
+
+
 DATA_FILE = 'data/eve.db'
 
-BUILD_PRODUCT_NAME = 'Nitrogen Fuel Block'
+BUILD_PRODUCT_NAME = 'Vexor'
 #BUILD_FACILITY = 'Large Ship Assembly Array'
 #BUILD_PRODUCT_NAME = 'Capital Cargo Bay'
 BUILD_FACILITY = 'STATION manufacturing'
-BUILD_PRODUCT_RUNS = 650
+BUILD_PRODUCT_RUNS = 1
 BUILD_PRODUCT_ME = 10
 BUILD_PRODUCT_TE = 20
 
 # ASSETS_LIST = '''Capital Jump Drive\t29\tCapital Construction Components\t
 # '''
 
-ASSETS_LIST = '''Tritanium\t2929173\tMineral\t\t\t4,546.37 m3
-Pyerite\t97720\tMineral\t\t\t245,869.82 m3
-Mexallon\t161164\tMineral\t\t\t1,410.84 m3
-Isogen\t6082\tMineral\t\t\t623.82 m3
-Nocxium\t23663\tMineral\t\t\t3,259.77 m3
-Zydrine\t6194\tMineral\t
-Megacyte\t39402\tMineral\t\t\t1,402.09 m3
+ASSETS_LIST = '''Tritanium\t559999\tMineral\t\t\t4,546.37 m3
+Pyerite\t119999\tMineral\t\t\t245,869.82 m3
+Mexallon\t36999\tMineral\t\t\t1,410.84 m3
 '''
+#Isogen\t6082\tMineral\t\t\t623.82 m3
+#Nocxium\t23663\tMineral\t\t\t3,259.77 m3
+#Zydrine\t6194\tMineral\t
+#Megacyte\t39402\tMineral\t\t\t1,402.09 m3
 
 ASSETS_LIST = ''
 
@@ -69,27 +73,6 @@ def create_asset_list(line_list):
 
     return asset_dict
 
-
-def write_material_list(material_list,
-                        asset_list,
-                        format_string):
-    '''
-    Outputs a formated material list
-    '''
-
-    for material_type_id, material_quantity in material_list.iteritems():
-        material_item = EveOnlineManufacturingJob(DATA_ACCESS_OBJECT,
-                                                  type_id=material_type_id)
-        material_name = material_item.data_access.get_inv_type(type_id=material_type_id)
-        if material_name is not None:
-            if material_type_id in asset_list.iterkeys():
-                asset_quantity = asset_list[material_type_id]
-            else:
-                asset_quantity = 0
-
-            print format_string % (material_name["type_name"], material_quantity, asset_quantity)
-
-
 def main():
     '''
     Main function for testing the classes
@@ -115,7 +98,7 @@ def main():
         if job.type_name[0:7] == 'Capital':
             job.blueprint_me_level = 10
             job.assembly_line = EveOnlineRamAssemblyLineTypes(DATA_ACCESS_OBJECT,
-                                                              assembly_line_type_name='Thukker Component Assembly Array')
+                                                              assembly_line_type_name='STATION manufacturing')
 
     # Recalculate after setting facility
     asset_dict = create_asset_list(ASSETS_LIST)
@@ -125,6 +108,7 @@ def main():
     manufacturing_job_list = building_job_chain.get_manufacturing_job_list()
 
     for job in manufacturing_job_list:
+
         print ("level: %d, runs %d (produced %d), %s (ME: %d, Facility: %s)" % (job.build_queue_level,
                                                                                 job.manufacturing_runs,
                                                                                 job.blueprint_produced_quantity,
@@ -135,32 +119,59 @@ def main():
 
     print
 
+    market = EveMarket(MarketAccessEveCentral())
+
+    total_material_cost = 0
+
     for mat_id, quant in building_job_chain.get_manufacturing_material_list().iteritems():
         e_material_item = EveOnlineManufacturingJob(DATA_ACCESS_OBJECT,
                                                     type_id=mat_id)
 
-        print "%s\t%d" % (e_material_item.type_name, quant)
+        mat_price = market.get_price(type_id=mat_id,
+                                     solar_system_id=30000142,
+                                     price_type='buy',
+                                     price_param='max')
+
+        mat_cost = (mat_price * quant)
+        total_material_cost += mat_cost
+
+        print "%s\tQuantity: %s\tPrice: %s\tCost: %s" % (e_material_item.type_name,
+                                    locale.format("%d", quant, grouping=True),
+                                    locale.format("%.2f", mat_price, grouping=True),
+                                    locale.format("%d", mat_cost, grouping=True))
+
+    print ("\nCost total: %s\n" % locale.format("%d", total_material_cost, grouping=True))
+
+    manufacturing_product_price = market.get_price(type_id=building_job_chain.type_id,
+                                                   solar_system_id=30000142,
+                                                   price_type='sell',
+                                                   price_param='min')
+
+    total_income = manufacturing_product_price * \
+                   building_job_chain.blueprint_produced_quantity * \
+                   building_job_chain.manufacturing_runs
+
+    produced_quantity = building_job_chain.blueprint_produced_quantity * \
+                        building_job_chain.manufacturing_runs
+
+    profit = total_income - total_material_cost
+
+    print ("Total material cost:\t%s\nSell price:\t%s\nProduced quantity:\t%s\nSelling product income:\t%s\nProfit:\t%s" % (locale.format("%d", total_material_cost, grouping=True),
+                                                                                   locale.format("%d", manufacturing_product_price, grouping=True),
+                                                                                   locale.format("%d", produced_quantity, grouping=True),
+                                                                                   locale.format("%d", total_income, grouping=True),
+                                                                                   locale.format("%d", profit, grouping=True)))
+    # refining_type = EveOnlineInvType(DATA_ACCESS_OBJECT,
+    #                                  type_name="White Glaze")
+    #
+    # refining_list = DATA_ACCESS_OBJECT.get_lst_mat_for_rfn(type_id=refining_type.type_id)
+    # print ("Refining list for %s:" % (refining_type.type_name))
+    #
+    # for mat in refining_list:
+    #     print ("material: %s, quantity: %d" % (DATA_ACCESS_OBJECT.get_inv_type(mat['material_type_id'])['type_name'],
+    #                                            mat['quantity']))
 
 
-    print
-
-    refining_type = EveOnlineInvType(DATA_ACCESS_OBJECT,
-                                     type_name="White Glaze")
-
-    refining_list = DATA_ACCESS_OBJECT.get_lst_mat_for_rfn(type_id=refining_type.type_id)
-    print ("Refining list for %s:" % (refining_type.type_name))
-
-    for mat in refining_list:
-        print ("material: %s, quantity: %d" % (DATA_ACCESS_OBJECT.get_inv_type(mat['material_type_id'])['type_name'],
-                                               mat['quantity']))
-
-    market = EveMarket(MarketAccessEveCentral())
-
-    print market.market_access_obj.get_marketstats(type_id=34,use_system=30000142)[0]['buy']['max']
-    print market.get_price(type_id=34,
-                           solar_system_id=30000142,
-                           price_type='buy',
-                           price_param='max')
 
 if __name__ == '__main__':
     DB_ACCESS_OBJECT = DBAccessSQLite(DATA_FILE)
